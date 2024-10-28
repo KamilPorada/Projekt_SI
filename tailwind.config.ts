56208bcd9c8025bd384d3db8725a25f9124161c5
Kamil0k <kamil.porada@onet.pl>
2024-10-28 20:30:16 +0100

    Setup structure of project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,39 @@
-import type { Config } from "tailwindcss";
+const tailwindColors = require('tailwindcss/colors')
 
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+const colors = {
+	...tailwindColors,
+	mainColor: '#1E3A8A',
+	secondaryColor: '#3B82F6',
+	accentColor: '#F97316',
+	backgroundColor: '#F3F4F6',
+}
+
+module.exports = {
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		colors,
+		extend: {
+			container: {
+				center: true,
+				padding: '2rem',
+				screens: {
+					sm: '100%',
+					md: '100%',
+					lg: '100%',
+					xl: '1200px',
+				},
+			},
+		},
+		screens: {
+			sm: '576px',
+			md: '768px',
+			lg: '992px',
+			xl: '1200px',
+		},
+	},
+	plugins: [],
+}
